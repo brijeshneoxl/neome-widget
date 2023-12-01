d42e30f3758ac5ecf8f66e27424791d9730c38eb
Brijesh Dobariya <brijesh@neoxl.com>
2023-12-01 13:20:29 +0530

    version 1.8

--- src/base/const.ts
+++ src/base/const.ts
@@ -1,4 +1,4 @@
-export const neomeFrameSrc = "https://web.orgbeat.com";
+export const neomeFrameSrc = "https://web.neome.ai";
 export const minWidgetWidth = 280;
 export const minWidgetHeight = 400;
 export const defaultFloatingWidth = 360;
