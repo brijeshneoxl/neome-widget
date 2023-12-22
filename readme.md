a4bdc5d6632af74acf35447ae5ff8256df34f1a7
Brijesh Dobariya <brijesh@neomenta.com>
2023-12-22 11:50:04 +0530

    neome/widget 0.0.3

--- readme.md
+++ readme.md
@@ -3,9 +3,9 @@
 </p>
 
 <p align="center">
-<a href="https://www.npmjs.com/package/ua-parser-js"><img src="https://img.shields.io/npm/v/@neome/widget.svg" alt="version"></a>
-<a href="https://www.npmjs.com/package/ua-parser-js"><img src="https://img.shields.io/npm/dw/@neome/widget.svg" alt="downloads"></a>
-<a href="https://www.jsdelivr.com/package/npm/ua-parser-js"><img src="https://data.jsdelivr.com/v1/package/npm/@neome/widget/badge" alt="cdn downloads"></a>
+<a href="https://www.npmjs.com/package/@neome/widget"><img src="https://img.shields.io/npm/v/@neome/widget.svg" alt="version"></a>
+<a href="https://www.npmjs.com/package/@neome/widget"><img src="https://img.shields.io/npm/dw/@neome/widget.svg" alt="downloads"></a>
+<a href="https://www.jsdelivr.com/package/npm/@neome/widget"><img src="https://data.jsdelivr.com/v1/package/npm/@neome/widget/badge" alt="cdn downloads"></a>
 </p>
 
 # Neome virtual assistant
--- src/main.test.tsx
+++ src/main.test.tsx
@@ -13,22 +13,22 @@ const local = "http://localhost:3000";
 const orgbeat = "https://web.orgbeat.com";
 const neome = "https://web.neome.ai";
 
-const url = orgbeat;
+const url = local;
 
 function App()
 {
   return <>
-    <LoadFloating
-      config={{
-        id: "floating1",
-        hostUrl: url,
-        filterEntId: "e-S85bGsw0sK18S98EJcuKiAke8",
-        selectGroupId: "g-eTaeh1Bsm8Tj6ivhOfDVsi3CS",
-        selectGroupIdBackButton: true,
-        allowPersonalChat: true,
-        signInWithoutCookie: false
-      }}
-    />
+    {/*<LoadFloating*/}
+    {/*  config={{*/}
+    {/*    id: "floating1",*/}
+    {/*    hostUrl: url,*/}
+    {/*    filterEntId: "e-S85bGsw0sK18S98EJcuKiAke8",*/}
+    {/*    selectGroupId: "g-eTaeh1Bsm8Tj6ivhOfDVsi3CS",*/}
+    {/*    selectGroupIdBackButton: true,*/}
+    {/*    allowPersonalChat: true,*/}
+    {/*    signInWithoutCookie: false*/}
+    {/*  }}*/}
+    {/*/>*/}
 
     <SimpleDiv />
     <SimpleDiv />
@@ -41,10 +41,10 @@ function App()
           id: "first",
           hostUrl: url,
           filterEntId: "e-S85bGsw0sK18S98EJcuKiAke8",
-          // selectGroupId: "g-eTaeh1Bsm8Tj6ivhOfDVsi3CS",
-          // selectGroupIdBackButton: true,
+          selectGroupId: "g-eTaeh1Bsm8Tj6ivhOfDVsi3CS",
+          selectGroupIdBackButton: true,
           allowStudio: true,
-          allowPersonalChat: true,
+          allowPersonalChat: false,
           signInWithoutCookie: true,
           userCredentials: [
             {
